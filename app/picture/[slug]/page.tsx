d619b55819fb74ef9a858774c904dad07729a5b7
Kane Clover <kane.clover@gmail.com>
2023-08-31 22:33:21 +0100

    image size + meta layout

--- app/picture/[slug]/page.tsx
+++ app/picture/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { pictures } from "@/assets/pictures";
 import { slugify } from "@/helpers/slugify";
 import Image from "next/image";
+import sizeOf from "image-size";
+import Link from "next/link";
 
 export async function generateStaticParams() {
   return pictures.map((pic) => ({
@@ -10,16 +12,22 @@ export async function generateStaticParams() {
 
 export default function Picture({ params }: { params: { slug: string } }) {
   const meta = pictures.find((pic) => slugify(pic.title) === params.slug);
+
+  if (!meta) return <div>Not found</div>;
+  const dimensions = sizeOf(`public/pictures/${meta?.id}.jpg`);
+
   return (
     <div>
-      <h1>Picture: {params.slug}</h1>
-      <pre> {JSON.stringify(meta, null, 2)}</pre>
-      <Image
-        src={`/pictures/${meta?.id}.jpg`}
-        width="100"
-        height="100"
-        alt="sdfsdf"
-      />
+      <h1 className="text-xl font-bold">{meta.title}</h1>
+      <Link href="/">
+        <Image
+          src={`/pictures/${meta.id}.jpg`}
+          width={dimensions.width}
+          height={dimensions.height}
+          alt={meta.title}
+        />
+      </Link>
+      <h4>{meta.description}</h4>
     </div>
   );
 }
