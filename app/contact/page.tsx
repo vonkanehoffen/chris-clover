3ec986cfbe471f203817410a7be808ff6b18a871
Kane Clover <kane.clover@gmail.com>
2023-09-15 19:32:52 +0100

    contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,21 @@
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="mx-8 text-lg text-center">
+      <h1 className="text-3xl my-8">Contact</h1>
+      <p>
+        If you are interested in purchasing originals or signed limited editions
+        of Chris Clover&rsquo;s work (
+        <a href="http://www.uisart.co.uk/" className="underline">
+          Giclee prints
+        </a>
+        ), or have any other queries or comments about this website, please
+        contact:
+      </p>
+      <p className="my-6 text-center">
+        <a href="mailto:chrisclover747@gmail.com" className="font-bold">
+          chrisclover747@gmail.com
+        </a>
+      </p>
     </div>
   );
 }
